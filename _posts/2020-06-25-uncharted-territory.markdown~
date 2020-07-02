9a04af73ec1cc4591db104ea20ae811676e0190f
Stephen Fell <stephen.fell@gmail.com>
2020-07-02 14:11:50 -0800

    newpost

---
+++ _posts/2020-06-25-uncharted-territory.markdown~
@@ -0,0 +1,71 @@
+---
+layout: post
+title: "Uncharted Territory" 
+date:   2020-06-25 08:00:00 -0800
+categories: python
+---
+
+
+
+Following [this example](https://github.com/HandsOnDataViz/chartjs-templates/tree/master/line-chart)
+
+Get rid of null values [modifying this example](https://jsfiddle.net/beaver71/u4wto8z1/)
+
+Parse dates [example](https://stackoverflow.com/questions/54334676/chart-js-format-date-in-label)
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  
+<!-- Load Chart.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+<script src="https://unpkg.com/chartjs-plugin-colorschemes@0.4.0/dist/chartjs-plugin-colorschemes.min.js"></script>
+
+<!-- Load Data Labels-->
+<!-- <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.7.0"></script> -->
+<!-- Load PapaParse to read csv files -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js"></script>
+
+<div><canvas id="chart-container" style="height: 400px; width: 100%"></canvas></div>
+
+<script src="/assets/script.js"></script>
+
+Get last not null value from array [link](https://stackoverflow.com/questions/49190873/get-the-last-non-null-element-of-an-array)
+
+Set up a Cron Job to process the data every 15 minutes [link](https://phoenixnap.com/kb/set-up-cron-job-linux)
+
+Run an HTTP server [link](https://docs.python.org/3/library/http.server.html)
+
+Do some debugging along the way.  `pdb.pm()` provides a really cool way to be able to see variables inside a function where the exception occurred. [Link](https://docs.python.org/3/library/pdb.html)
+
+
+# Display Last Values
+ <table style="width:100%">
+  <tr>
+    <th>T0</th>
+    <th>T1</th>
+    <th>T2</th>
+    <th>T3</th>
+    <th>TC</th>
+  </tr>
+  <tr>
+    <td style="text-align:center"><div id="T0"></div></td>
+    <td style="text-align:center"><div id="T1"></div></td>
+    <td style="text-align:center"><div id="T2"></div></td>
+    <td style="text-align:center"><div id="T3"></div></td>
+    <td style="text-align:center"><div id="T4"></div></td>
+  </tr>
+</table> 
+
+<br><br>
+
+T0:
+<div id="T0"></div>
+T1:
+<div id="T1"></div>
+T2:
+<div id="T2"></div>
+T3:
+<div id="T3"></div>
+TC:
+<div id="T4"></div>
---
+++ assets/script.js
@@ -0,0 +1,136 @@
+var TITLE = 'Temperature Data';
+
+var X_AXIS = 'Time';  // x-axis label and label in tooltip
+var Y_AXIS = 'Temperature (deg F)'; // y-axis label and label in tooltip
+
+var BEGIN_AT_ZERO = false;  // Should x-axis start from 0? `true` or `false`
+
+var SHOW_GRID = true; // `true` to show the grid, `false` to hide
+var SHOW_LEGEND = true; // `true` to show the legend, `false` to hide
+
+// https://jsfiddle.net/beaver71/u4wto8z1/
+// Modified a function found on jsfiddle to do the opposite
+function zeroToNull(array) {
+  return array.map(function(v) { 
+    if (v==0) return null; else return v;
+  });
+}
+
+
+$(document).ready(function() {
+
+  // Read data file and create a chart
+  $.get('/assets/df1.csv', function(csvString) {
+
+    var data = Papa.parse(csvString).data;
+    var timeLabels = data.slice(1).map(function(row) { return row[0]; });
+
+    var datasets = [];
+    for (var i = 1; i < data[0].length; i++) {
+      datasets.push(
+        {
+          label: data[0][i], // column name
+          data: zeroToNull(data.slice(1).map(function(row) {return row[i]})), // data in that column, and get rid of zeros
+          fill: false, // `true` for area charts, `false` for regular line charts
+          showLine: false
+        }
+      )
+    }
+
+$().ready(function() {
+  $("#T0").html(datasets[0].data.filter(x => x != null).slice(-1)[0]),
+  $("#T1").html(datasets[1].data.filter(x => x != null).slice(-1)[0]),
+  $("#T2").html(datasets[2].data.filter(x => x != null).slice(-1)[0]),
+  $("#T3").html(datasets[3].data.filter(x => x != null).slice(-1)[0]),
+  $("#T4").html(datasets[4].data.filter(x => x != null).slice(-1)[0])
+});    
+
+
+
+    
+    
+    // Get container for the chart
+    var ctx = document.getElementById('chart-container').getContext('2d');
+
+    new Chart(ctx, {
+      type: 'line',
+
+      data: {
+        labels: timeLabels,
+        datasets: datasets,
+      },
+      
+      options: {
+        title: {
+          display: true,
+          text: TITLE,
+          fontSize: 14,
+        },
+        legend: {
+          display: SHOW_LEGEND,
+        },
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: X_AXIS !== '',
+              labelString: X_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                return value.toLocaleString();
+              },
+            },
+            type: 'time',
+            display: true,
+            time: {
+              parser: 'YYYY-MM-DD HH:mm:ss',
+              tooltipFormat: 'll HH:mm',
+              unit: 'day',
+              unitStepSize: 1,
+              displayFormats: {'day': 'MM/DD/YYYY'},
+              min: '2020-06-29 12:00',
+              //max: '06/10/2020 12:00'
+            }
+          }],
+          yAxes: [{
+            beginAtZero: false,
+            scaleLabel: {
+              display: Y_AXIS !== '',
+              labelString: Y_AXIS
+            },
+            gridLines: {
+              display: SHOW_GRID,
+            },
+            ticks: {
+              beginAtZero: BEGIN_AT_ZERO,
+              callback: function(value, index, values) {
+                return value.toLocaleString()
+              }
+            }
+          }]
+        },
+        tooltips: {
+          displayColors: false,
+          callbacks: {
+            label: function(tooltipItem, all) {
+              return all.datasets[tooltipItem.datasetIndex].label
+                + ': ' + tooltipItem.yLabel.toLocaleString();
+            }
+          }
+        },
+        plugins: {
+          colorschemes: {
+            scheme: 'brewer.DarkTwo5'
+          }
+        }
+      }
+    });
+
+  });
+
+});
+
